a319cff077f0fb8f4a2871782fd4359bb3045f43
moohalem <mohalem.public@gmail.com>
2025-09-29 11:22:14 +0700

    Change API server port from 8080 to 9090.

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -92,7 +92,7 @@ async fn main() -> std::io::Result<()> {
             .route("/", web::get().to(serve_index))
             .route("/static/app.js", web::get().to(serve_js))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 9090))?
     .run()
     .await
 }
